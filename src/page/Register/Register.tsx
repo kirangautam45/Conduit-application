2ebd94ceb1587c2a03babfa41259d2e0f3df2776
kiran gautam <kirangautam225@gmail.com>
2023-03-11 15:37:50 +0545

    HeadePill optimise

--- src/page/Register/Register.tsx
+++ src/page/Register/Register.tsx
@@ -1,9 +1,10 @@
 import { Link } from "react-router-dom";
-
+import Home from "../Home/Home";
 
 const Register = () => {
   return (
     <div>
+      <Home />
       <h1>This is Register Page</h1>
       <Link to={"/"}>Home </Link>
       <br />
