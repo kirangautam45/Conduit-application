857a5f65114abe7cbb05643c810f03a64ef04042
kiran gautam <kirangautam225@gmail.com>
2023-04-03 11:40:21 +0545

    footer and pagination managed

--- src/page/Dashboard/Dashboard.tsx
+++ src/page/Dashboard/Dashboard.tsx
@@ -30,35 +30,36 @@ const Dashboard = () => {
       <NavBar />
       <Banner />
 
-      <div className={style.wrapper}>
-        <div className={style.leftSide}>
-          {articles.length ? (
-            articles.map((data, index) => (
-              <ArticleInfo
-                key={index}
-                username={data.author.username}
-                tag={data.tagList}
-                count={data.favoritesCount}
-                slug={data.slug}
-                title={data.title}
-                description={data.description}
-                createdAt={data.createdAt}
-                demo={data.author.image}
-              />
-            ))
-          ) : (
-            <div> Article Loading...</div>
-          )}
-        </div>
+      {isLoading && !isSuccess && <div>Article Loading... </div>}
+      {isSuccess && (
+        <>
+          <div className={style.wrapper}>
+            <div className={style.leftSide}>
+              {articles.map((data, index) => (
+                <ArticleInfo
+                  key={index}
+                  username={data.author.username}
+                  tag={data.tagList}
+                  count={data.favoritesCount}
+                  slug={data.slug}
+                  title={data.title}
+                  description={data.description}
+                  createdAt={data.createdAt}
+                  demo={data.author.image}
+                />
+              ))}
+              <div className={style.pagination}>
+                {!isLoading && isSuccess && <Pagination />}
+              </div>
+            </div>
 
-        <div className={style.rightSide}>
-          <Tags />
-        </div>
-      </div>
-      <div className={style.pagination}>
-        {isLoading && !isSuccess && <>Article Loading... </>}
-        {!isLoading && isSuccess && <Pagination />}
-      </div>
+            <div className={style.rightSide}>
+              <Tags />
+            </div>
+          </div>
+        </>
+      )}
+
       <Footer />
     </>
   );
